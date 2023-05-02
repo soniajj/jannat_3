b6633112a60fb256383eb6048060879681f9c95d
Jannatul Ferdous <Jannatul Ferdous@192.168.0.236>
2023-05-02 12:18:33 -0500

    Created new TestNG class for testing purposes.

---
+++ src/test/java/testsenarios/NewTest_2.java
@@ -0,0 +1,22 @@
+package testsenarios;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest_2 {
+  @Test
+  public void f() {
+	  
+	  WebDriver driver;	
+	  driver = new ChromeDriver();
+	  
+	  driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");   
+	    
+	  
+	  
+	  
+	  
+	  
+  }
+}
